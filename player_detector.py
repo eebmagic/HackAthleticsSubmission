4b95d9fb453d406f2ddc1c1a0deba59f11bde1b7
Kieran Black <kieranlblack@outlook.com>
2020-02-23 00:17:55 -0500

    the work continues

--- player_detector.py
+++ player_detector.py
@@ -1,90 +1,54 @@
 import cv2
+from imutils.object_detection import non_max_suppression
 import numpy as np
-from frame_classifier.orientation_detector import get_lines
 from people_classifier.classify_team import classify_person
 from util import get_frame, format_frame
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 video = cv2.VideoCapture('media/1904-GATC-CONT-vs-PATE.mp4')
 
 video.set(cv2.CAP_PROP_POS_FRAMES, get_frame('2:10'))
 
 success, frame = video.read()
-(h, w) = frame.shape[:2]
-
-count = 0
-success = True
-idx = 0
 
 field_sample = cv2.imread('media/field_sample.png') # load the base field image
 field_hsv = cv2.cvtColor(field_sample, cv2.COLOR_BGR2HSV) # convert the field image into hsv
 mu, sig = cv2.meanStdDev(field_hsv) # find the mean colour of the base field image
-devs = 14 # tolerance
+devs = 16 # tolerance
 
 dark_field_sample = cv2.imread('media/dark_field_sample.png')
 dark_field_hsv = cv2.cvtColor(dark_field_sample, cv2.COLOR_BGR2HSV)
 dmu, dsig = cv2.meanStdDev(dark_field_hsv)
 ddevs = 14
 
-def generate_line_mask(lines):
-    mask = np.zeros((h, w), np.uint8)
-    mask.fill(255)
-
-    for rho, theta in lines:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a * rho * 10
-        y0 = b * rho * 10
-        x1 = int(x0 + 1000 * (-b) * 10)
-        y1 = int(y0 + 1000 * a * 10)
-        x2 = int(x0 - 1000 * (-b) * 10)
-        y2 = int(y0 - 1000 * a * 10)
-
-        cv2.line(mask, (x1, y1), (x2, y2), 0, 3)
-
-    cv2.imshow('lines', mask)
-    return mask
-
 if __name__ == '__main__':
     while video.isOpened():
-        (h, w) = frame.shape[:2]
+        orig = frame.copy()
 
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # convert frame into hsv
 
-        line_mask = generate_line_mask(get_lines(format_frame(frame, 10)))
         field_mask = cv2.inRange(hsv, mu - devs * sig, mu + devs * sig)
         dark_field_mask = cv2.inRange(hsv, dmu - ddevs * dsig, dmu + ddevs * dsig)
-        masked = cv2.bitwise_and(frame, frame, mask=cv2.bitwise_not(field_mask | dark_field_mask | line_mask))
+        masked = cv2.bitwise_and(frame, frame, mask=cv2.bitwise_not(field_mask | dark_field_mask))
 
         # convert to hsv and to grayscale
         frame_gray = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
 
-        # some magic to get a better output frame
-        kernel = np.ones((13, 13), np.uint8)
-        _, thresh = cv2.threshold(frame_gray, 25, 255, 0)
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+        # detect people in the image
+        # returns the bounding boxes for the detected objects
+        boxes, weights = hog.detectMultiScale(format_frame(frame_gray, 0.5), winStride=(8,8))
 
-        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # find contours in the threshold frame
-        cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
 
-        for c in contours:
-            x, y, w, h = cv2.boundingRect(c)
+        print(boxes)
 
-            # time to find the players
-            if h >= 1.2 * w and h <= 5 * w:
-                if w > 50 and h >= 50:
-                    dx = idx + 1
-                    player_img = frame[y:(y + h), x:(x + w)]
-                    # here we would call classify team
-                    # classify_person(player_img)
-
-                    cv2.rectangle(frame, (x, y),(x + w, y + h), (255, 0, 0), 3)
-
+        for (xA, yA, xB, yB) in boxes:
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
         cv2.imshow('output', frame)
         cv2.imshow('masked', masked)
-        cv2.imshow('gray', frame_gray)
-        cv2.imshow('thresh', thresh)
-        count += 1
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
--- util.py
+++ util.py
@@ -6,13 +6,13 @@ def get_frame(time):
     mini, sec = list(map(int, time.split(':')))
     return FRAME_RATE * (mini * 60 + sec)
 
-def format_frame(image, percent=20):
+def format_frame(image, factor=0.2):
     # reduce color to grayscale
     # inputFrame = cv2.cvtColor(inputFrame, cv2.COLOR_BGR2GRAY)
 
     # reduce resolution
-    width = int(image.shape[1] * percent / 100)
-    height = int(image.shape[0] * percent / 100)
+    width = int(image.shape[1] * factor)
+    height = int(image.shape[0] * factor)
     dim = (width, height)
 
     return cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
