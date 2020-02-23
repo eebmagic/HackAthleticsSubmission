47b0d7fadb1f320ddbcb17bee24f9e16f5ee24eb
Kieran Black <kieranlblack@outlook.com>
2020-02-22 21:18:36 -0500

    commit before munchies

--- player_detector.py
+++ player_detector.py
@@ -15,31 +15,37 @@ idx = 0
 field_sample = cv2.imread('media/field_sample.png') # load the base field image
 field_hsv = cv2.cvtColor(field_sample, cv2.COLOR_BGR2HSV) # convert the field image into hsv
 mu, sig = cv2.meanStdDev(field_hsv) # find the mean colour of the base field image
-devs = 15 # tolerance
+devs = 14 # tolerance
+
+dark_field_sample = cv2.imread('media/dark_field_sample.png')
+dark_field_hsv = cv2.cvtColor(dark_field_sample, cv2.COLOR_BGR2HSV)
+dmu, dsig = cv2.meanStdDev(dark_field_hsv)
+ddevs = 14
 
 if __name__ == '__main__':
     while video.isOpened():
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # convert frame into hsv
 
         field_mask = cv2.inRange(hsv, mu - devs * sig, mu + devs * sig)
-        masked = cv2.bitwise_and(frame, frame, mask=field_mask)
+        dark_field_mask = cv2.inRange(hsv, dmu - ddevs * dsig, dmu + ddevs * dsig)
+        masked = cv2.bitwise_and(frame, frame, mask=cv2.bitwise_not(field_mask | dark_field_mask))
 
         # convert to hsv and to grayscale
-        res_bgr = cv2.cvtColor(masked, cv2.COLOR_HSV2BGR)
-        res_gray = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+        frame_gray = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
 
         # some magic to get a better output frame
-        kernel = np.ones((13, 13), np.uint8)
-        thresh = cv2.threshold(res_gray, 127, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        kernel = np.ones((10, 10), np.uint8)
+        _, thresh = cv2.threshold(frame_gray, 127, 255, 0)
         thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # find contours in the threshold frame
+        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # find contours in the threshold frame
+        cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
 
         for c in contours:
             x, y, w, h = cv2.boundingRect(c)
 
             # time to find the players
-            if h >= 1.2 * w:
+            if h >= 1.5 * w and h <= 4 * w:
                 if w > 50 and h >= 50:
                     dx = idx + 1
                     player_img = frame[y:(y + h), x:(x + w)]
@@ -50,6 +56,7 @@ if __name__ == '__main__':
 
 
         cv2.imshow('output', frame)
+        cv2.imshow('masked', masked)
         count += 1
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
